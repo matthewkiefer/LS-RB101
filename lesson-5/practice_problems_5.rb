58ed5e6a168411fc00fcfd8b3c6652346c1a41f3
Matt Kiefer <mattkiefer@gmail.com>
2021-01-19 09:01:45 -0500

    solved problem 5 but would like to work on #reduce

--- lesson-5/practice_problems_5.rb
+++ lesson-5/practice_problems_5.rb
@@ -27,3 +27,54 @@ end
 
 p sorted
    
+# problem 3
+arr1 = ['a', 'b', ['c', ['d', 'e', 'f', 'g']]]
+puts arr1[2][1][3] == "g"
+
+arr2 = [{first: ['a', 'b', 'c'], second: ['d', 'e', 'f']}, {third: ['g', 'h', 'i']}]
+puts arr2[1][:third][0] == "g"
+
+arr3 = [['abc'], ['def'], {third: ['ghi']}]
+puts arr3[2][:third][0][0] == "g"
+
+hsh1 = {'a' => ['d', 'e'], 'b' => ['f', 'g'], 'c' => ['h', 'i']}
+puts hsh1["b"][1] == "g"
+
+hsh2 = {first: {'d' => 3}, second: {'e' => 2, 'f' => 1}, third: {'g' => 0}}
+puts hsh2[:third].keys[0] == "g"
+
+# problem 4
+arr1 = [1, [2, 3], 4]
+arr1[2] = 3
+p arr1
+
+arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+arr2[2] = 4
+p arr2
+
+hsh1 = {first: [1, 2, [3]]}
+hsh1[:first][2][0] = 4
+p hsh1
+
+hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+hsh2[["a"]][:a][2] = 4
+p hsh2
+
+# problem 5
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+age = 0
+total_age = munsters.each do |value, key|
+    age += key["age"] if key["gender"] == "male"
+end
+puts age
+
+
+  # munster.reduce do |age, munster["age"]|
+  #   munster["age"]
---
+++ small-problems/easy-2.rb
@@ -0,0 +1,4 @@
+# easy-2.rb
+
+# How Old is Teddy?
+puts "Teddy is #{rand(20..200)} years old!"
