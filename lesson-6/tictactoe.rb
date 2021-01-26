a32df401059d668880b762b665aaac5897131345
Matt Kiefer <mattkiefer@gmail.com>
2021-01-26 08:52:09 -0500

    scorekeeping functioning

--- lesson-6/tictactoe.rb
+++ lesson-6/tictactoe.rb
@@ -7,6 +7,7 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+VICTORIES_TO_WIN = 3
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -31,24 +32,25 @@ def display_board(brd)
 end
 # rubocop:enable Metrics/AbcSize
 
-def initialize_board
-  new_board = {}
+def initialize_board(brd = Hash.new(0))
+  new_board = brd
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  # reset_score_if_victor(new_board)
   new_board
 end
 
+def wipe_board(brd)
+  (1..9).each { |num| brd[num] = INITIAL_MARKER }
+end
+
+
+# def reset_score_if_victor(brd)
+#   if (brd.key?(:player_score) || brd.key(:computer_score)) &&
+#     brd[:player_score] < VICTORES_TO_WIN && brd[:computer_score] < VICTORES_TO_WIN
+#   end
+# end
+
 def joinor(arr, punc = ",", separator = "or")
-  # return arr[0] if arr.size == 1
-  # return "#{arr[0]} #{separator} #{arr[1]}" if arr.size == 2
-  # joined = ""
-  # arr.each_with_index do |element, idx|
-  #   if idx == (arr.size - 1)
-  #     joined << "#{separator} #{element}"
-  #   else
-  #     joined << "#{element}#{punc}"
-  #   end
-  # end
-  # joined
   case arr.size
   when 1 
     arr[0]
@@ -89,6 +91,11 @@ def someone_won?(brd)
   !!detect_winner(brd)
 end
 
+def grand_winner?(brd)
+  brd[:player_score] == VICTORIES_TO_WIN ||
+  brd[:computer_score] == VICTORIES_TO_WIN
+end
+
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(PLAYER_MARKER) == 3 # splat line; same below
@@ -100,8 +107,19 @@ def detect_winner(brd)
   nil
 end
 
+def increment_score(brd)
+  winner = detect_winner(brd)
+  winner == "Player" ? brd[:player_score] += 1 : brd[:computer_score] += 1
+end
+
+def reset_score(brd)
+  initialize_board
+end
+
+####### Main Logic Here #######
+
+board = initialize_board
 loop do
-  board = initialize_board
 
   loop do
     display_board(board)
@@ -116,14 +134,57 @@ loop do
   display_board(board)
 
   if someone_won?(board)
+    increment_score(board)
     prompt "#{detect_winner(board)} won!"
+    # binding.pry
+    prompt "The score is now player: #{board[:player_score]}, computer #{board[:computer_score]}"
   else
     prompt "It's a tie!"
+  end
+
+  if grand_winner?(board)
+    prompt  "#{detect_winner(board)} reached the score of #{VICTORIES_TO_WIN}" +
+            " and is the GRAND WINNER!"
+    reset_score(board)
   end
 
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?("y")
+  wipe_board(board)
 end
 
 prompt "Thanks for playing. Bye now!"
+
+# 
+# 2. Keep score
+# Playing to five. No instance or global variables.
+# So, must pass the score each time.
+
+# 1. Display the initial empty 3x3 board.
+#   2.5 Display score up top
+#   Can I add score to board?
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display winner and. 
+#   If 5 times, grand winner
+# 6. If board is full, display tie.
+# 7. If neither winner nor board is full, go to #2
+# 8. Play again?
+# 9. If yes, go to #1
+# 10. Good bye!
+
+# Initialize: pass the board to it (with a defualt param); check score def
+#   -grand_winner?
+
+#   on init, don't set score but don't reset
+#   ----way to have the default value be 0 of a new hash key
+#   Init sets the default value
+#   win condition adds one
+#   in someone_won? 
+
+# added default values, but still need to pass the board back at the start to keep the hash from 
+# being overwritten
+
+# To improve on score keeping: announce goal at start
